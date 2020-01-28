c8afb6ecbeb6a70d1f8f1fd6537164e90a76c59a
Vitor-Lopes <lopesssbr@gmail.com>
2020-01-28 12:42:06 -0300

    exercícios de leitura de código feitos

---
+++ semana3/aula2-logica-booleana/index.js
@@ -0,0 +1,69 @@
+
+//INTERPRETAÇÃO DE CÓDIGO
+
+//EXERCÍCIO1
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2 //true
+
+// let resultado = bool1 && bool2 && bool3
+// console.log("a. ", resultado)//false
+
+// resultado = (bool2 || bool1) && !bool3
+// console.log("b. ", resultado)//false
+
+// resultado = !resultado && (bool1 || bool1)
+// console.log("c. ", resultado)//true
+
+// resultado = (resultado && (!bool1 || bool2)) && !bool3
+// console.log("d. ", resultado)//false
+
+// console.log("e. ", typeof resultado)//boolean
+
+//EXERCÍCIO2
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2
+
+// let resultado = bool1 && bool2 && bool3
+// console.log("a. ", resultado)//false
+
+// resultado = (bool2 || bool1) && !bool3
+// console.log("b. ", resultado)//false
+
+// resultado = !resultado && (bool1 || bool1)
+// console.log("c. ", resultado)//true
+
+// resultado = (resultado && (!bool1 || bool2)) && !bool3
+// console.log("d. ", resultado)//false
+
+// console.log("e. ", typeof resultado)//boolean
+
+/*2.    A- O que é array e como se declara em JS?
+        R- array é uma variável que pode receber mais de um valor 
+           simultaneamente
+        B- Qual o index inicial de um array?
+        R- [i] começando com zero
+        C- Como se determinar o tamanho do array?
+        R- utilizando .lenght
+        D- Indique todas as mensagens impressas no console.
+        R- Indiquei em forma de comentários ao lado de cada console.log()
+            1-  false
+                false
+                true
+                false
+                boolean
+
+            2-  false
+                false
+                true
+                false
+                boolean
+*/
+//----------------------------
+
+
+
+
+
+
