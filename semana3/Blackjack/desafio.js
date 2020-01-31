08d71651d32b73c05437790e714897695dc32a05
Vitor-Lopes <lopesssbr@gmail.com>
2020-01-31 16:40:09 -0300

    brigatório completo. esafios incompletos

---
+++ semana3/Blackjack/desafio.js
@@ -0,0 +1,42 @@
+// import comprarCarta from './naoMexer.js'
+// // NÃO REMOVA ESTA LINHA
+
+// /////___________BLACKJACK____________/////
+
+// console.log("Bem vindo ao jogo de Blackjack!")
+
+// if (confirm("Quer iniciar uma nova rodada?")) {
+//   console.log("iniciando uma nova rodada:")
+
+//   const user1 = prompt("Digite seu nome")
+//   var cartaUsuario
+// //   const carta1 = Object.values(comprarCarta())
+// //   const carta2 = Object.values(comprarCarta())
+//    for (let i = 0; i <= 2; i++) {
+//       const carta = Object.values(comprarCarta())
+//       cartaUsuario.concat(carta)
+      
+//    }
+
+//   var cartaUsuario = carta1.concat(carta2)
+//   console.log
+
+//   console.log(`${user1} - cartas: ${cartaUsuario[0]} ${carta2.texto} - pontuação ${carta1.valor + carta2.valor}`)
+
+//   const IA = "Computador"
+//   const carta1IA = Object.values(comprarCarta())
+//   const carta2IA = Object.values(comprarCarta())
+
+//   console.log(`${IA} - cartas: ${carta1IA.texto} ${carta2IA.texto} - pontuação ${carta1IA.valor + carta2IA.valor}`)
+
+//   if (carta1.valor + carta2.valor === carta1IA.valor + carta2IA.valor) {
+//     console.log("Empate!")
+//   } else if (carta1 + carta2 < carta1IA + carta2IA) {
+//     console.log("Computador Ganhou!")
+//   } else {
+//     console.log("Usuário ganhou!")
+
+//   }
+// } else {
+//   console.log("O jogo acabou =(")
+// }
