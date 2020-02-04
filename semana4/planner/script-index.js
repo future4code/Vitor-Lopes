00e7b38a0cb8cbfea5f76d870a616effef96c58f
Vitor-Lopes <lopesssbr@gmail.com>
2020-02-04 16:56:47 -0300

    exercício obrigatório ok-Desafios incompletos

---
+++ semana4/planner/script-index.js
@@ -0,0 +1,34 @@
+function criarTarefa() {
+    let item = document.getElementById('novaTarefa').value
+    let day = document.getElementById('diaDaSemana').value
+    let tarefaNova = document.createElement('li')
+
+    if (item === "") {
+        alert("Preencha o campo Nova Tarefa para adicionar um item.")
+    } else {
+        tarefaNova.setAttribute("class", "tarefa")
+        // tarefaNova.setAttribute("onclick", )
+
+        tarefaNova.textContent = item
+        if (day == 'segunda') {
+            document.getElementById('segunda').appendChild(tarefaNova)
+        } else if (day == 'terca') {
+            document.getElementById('terca').appendChild(tarefaNova)
+        } else if (day == 'quarta') {
+            document.getElementById('quarta').appendChild(tarefaNova)
+        } else if (day == 'quinta') {
+            document.getElementById('quinta').appendChild(tarefaNova)
+        } else if (day == 'sexta') {
+            document.getElementById('sexta').appendChild(tarefaNova)
+        } else if (day == 'sabado') {
+            document.getElementById('sabado').appendChild(tarefaNova)
+        } else if (day == 'domingo') {
+            document.getElementById('domingo').appendChild(tarefaNova)
+        }
+        document.getElementById('novaTarefa').value = ""
+    }
+}
+
+function limparTarefa() {
+    location.reload()
+}
