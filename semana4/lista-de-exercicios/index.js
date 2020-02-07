5fe35f3cc76e52d0d572130ee1818ab0cf8bb083
Vitor-Lopes <lopesssbr@gmail.com>
2020-02-07 18:06:46 -0300

    funções de array - ex 3 concluído

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -221,10 +221,27 @@ exerciciosDeObjetos4()
 
 
 
-
-
-
 ///////////___________EXERCÍCIO 3____________///////////
+
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+function regraMontanhaRussa () {
+    
+    const regraAltura = pessoas.filter((tamanho, index, array) => {
+        return (tamanho.altura > 1.5 && tamanho.idade > 14 && tamanho.idade < 60)
+    }) 
+    console.log(regraAltura)
+}
+regraMontanhaRussa()
+
+
+
 ///////////___________EXERCÍCIO 4____________///////////
 ///////////___________EXERCÍCIO 5____________///////////
 
