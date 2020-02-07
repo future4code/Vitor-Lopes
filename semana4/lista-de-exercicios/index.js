ef879c2c1da9dc8213d951778c58e2cc12a457d8
Vitor-Lopes <lopesssbr@gmail.com>
2020-02-07 11:04:40 -0300

    lógica da programação-Ex3 concluído

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -8,6 +8,7 @@
 ///////////EXERCÍCIO DE LÓGICA DE PROGRAMAÇÃO///////////
 ///////////___________EXERCÍCIO 1____________///////////
 ///////////___________EXERCÍCIO 2____________///////////
+
 /* 
     A- false
     B- false
@@ -15,8 +16,41 @@
     D- true
     E- true
 */
-///////////___________EXERCÍCIO 3____________///////////
 
+///////////___________EXERCÍCIO 3____________///////////
+/**
+    o código deveria ter um valor para quantidadeDeNumerosPares por ser const.
+    faltou incrementar 2 ao valor de i pois do jeito que estava, 0 x 2 sempre será 0
+ */
+function LógicaDeProgramacaoEx03() {
+    const quantidadeDeNumerosPares = 2
+    let i = 0
+    while (i <= quantidadeDeNumerosPares) {
+        console.log(i)
+        i += 2
+    }
+}
+
+///////////___________EXERCÍCIO 4____________///////////
+///////////___________EXERCÍCIO 5____________///////////
+
+
+///////////_______EXERCÍCIO DE FUNÇÕES_______///////////
+///////////___________EXERCÍCIO 1____________///////////
+///////////___________EXERCÍCIO 2____________///////////
+
+///////////_______EXERCÍCIO DE OBJETOS_______///////////
+///////////___________EXERCÍCIO 1____________///////////
+///////////___________EXERCÍCIO 2____________///////////
+///////////___________EXERCÍCIO 3____________///////////
+///////////___________EXERCÍCIO 4____________///////////
+
+///////////__EXERCÍCIO DE FUNÇÕES DE ARRAY___///////////
+///////////___________EXERCÍCIO 1____________///////////
+///////////___________EXERCÍCIO 2____________///////////
+///////////___________EXERCÍCIO 3____________///////////
+///////////___________EXERCÍCIO 4____________///////////
+///////////___________EXERCÍCIO 5____________///////////
 
 
 
