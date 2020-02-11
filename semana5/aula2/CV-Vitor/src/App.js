870ba9bbe76e22b8fa7b4c14deddf2a91609d641
Vitor-Lopes <lopesssbr@gmail.com>
2020-02-11 14:56:30 -0300

    componentes 1 e 2 feitos

--- semana5/aula2/CV-Vitor/src/App.js
+++ semana5/aula2/CV-Vitor/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  BigCard from './Components/BigCard/BigCard'
+import  SmallCard from './Components/SmallCard/SmallCard'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <BigCard titulo='Vitor Soares Lopes' texto='Oi eu sou o Vitor' imagem='https://codinginfinite.com/wp-content/uploads/2019/05/maxresdefault-1.jpg' /> */}
+    <SmallCard titulo='Email:' texto='emailaqui@email.com' imagem='https://codinginfinite.com/wp-content/uploads/2019/05/maxresdefault-1.jpg' />
+
     </div>
   );
 }
