0e4b394ec7aed9b3172651a9333225c000d80125
vitor-slopes <vitor_slopes@hotmail.com>
2020-03-06 15:24:42 -0300

    criar playlist e lista de playlists criadas

---
+++ semana7/projeto-semanal/spotif4/src/Components/CreatePlaylist.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+
+const CreatePlaylist = styled.div`
+  width: 86vw;
+  height: 76vh;
+  background-color: #a29bfe;
+  display: flex;
+  padding: 2vh 2vw;
+`
+
+
+
+
+export default class FooterMediaPlayer extends Component {
+
+  render() {
+    return (
+      <CreatePlaylist>
+        CreatePlaylist
+      </CreatePlaylist>
+    )
+  }
+}
---
+++ semana7/projeto-semanal/spotif4/src/Components/Sidebar.js
@@ -0,0 +1,37 @@
+// import React, { Component } from 'react'
+// import axios from 'axios'
+// import styled from 'styled-components'
+
+
+
+
+// export default class Sidebar extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       openCreatePlaylist: 'GridMusic',
+//     }
+//   }
+
+
+//   trocaDePagina = () => { //método para trocar de página
+//     if (this.state.openCreatePlaylist === 'GridMusic') {
+//       this.setState({ openCreatePlaylist: 'lista' })
+//     } else {
+//       this.setState({ openCreatePlaylist: 'GridMusic' })
+//     }
+//   }
+
+
+
+//   render() {
+//     return (
+//       <ComponentWrapper>
+//         <ButtomDiv>Início</ButtomDiv>
+//         <ButtomDiv>Buscar</ButtomDiv>
+//         <ButtomDiv onClick={this.state.trocaDePagina}>>Playlists</ButtomDiv>
+
+//       </ComponentWrapper>
+//     )
+//   }
+// }
