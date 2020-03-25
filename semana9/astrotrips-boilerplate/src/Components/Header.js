999060ef2074f7f6f6e6568d8452addf0598c281
vitor-slopes <vitor_slopes@hotmail.com>
2020-03-25 18:34:40 -0300

    otimizei o código do Header

--- semana9/astrotrips-boilerplate/src/Components/Header.js
+++ semana9/astrotrips-boilerplate/src/Components/Header.js
@@ -12,57 +12,21 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import { connect } from "react-redux";
 import { push } from "connected-react-router";
 import styled from 'styled-components'
-import {routes} from '../containers/Router/index'
+import { routes } from '../containers/Router/index'
 
-
-const Image = styled.img`
+const Logo = styled.img`
 height: 100px;
 `
 
 class PrimarySearchAppBar extends React.Component {
-  state = {
-    anchorEl: null,
-    mobileMoreAnchorEl: null,
-  };
-
-  handleProfileMenuOpen = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleMenuClose = () => {
-    this.setState({ anchorEl: null });
-    this.handleMobileMenuClose();
-  };
-
-  handleMobileMenuOpen = event => {
-    this.setState({ mobileMoreAnchorEl: event.currentTarget });
-  };
-
-  handleMobileMenuClose = () => {
-    this.setState({ mobileMoreAnchorEl: null });
-  };
 
   render() {
-    const { anchorEl, mobileMoreAnchorEl } = this.state;
-    const isMenuOpen = Boolean(anchorEl);
-
-    const renderMenu = (
-      <Menu
-        anchorEl={anchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={isMenuOpen}
-        onClose={this.handleMenuClose}
-      >
-        <MenuItem onClick={this.handleMenuClose}>Minha Conta</MenuItem>
-      </Menu>
-    );
 
     return (
       <div>
         <AppBar position="static">
           <Toolbar>
-            <Image onClick={this.props.goToHomePage} src='https://user-images.githubusercontent.com/56808066/77550367-ce600780-6e8f-11ea-8aa1-a666bb51626d.png'></Image>
+            <Logo onClick={this.props.goToHomePage} src='https://user-images.githubusercontent.com/56808066/77550367-ce600780-6e8f-11ea-8aa1-a666bb51626d.png'></Logo>
             <div>
               <div>
                 <SearchIcon />
@@ -77,8 +41,6 @@ class PrimarySearchAppBar extends React.Component {
             <div />
             <div>
               <IconButton
-                aria-owns={isMenuOpen ? 'material-appbar' : undefined}
-                aria-haspopup="true"
                 onClick={this.props.goToTripsList}
                 color="inherit"
               >
@@ -87,7 +49,6 @@ class PrimarySearchAppBar extends React.Component {
             </div>
           </Toolbar>
         </AppBar>
-        {renderMenu}
       </div>
     );
   }
@@ -103,7 +64,6 @@ function mapDispatchToProps(dispatch) {
     goToTripsList: () => dispatch(push(routes.tripsCreate)),
   };
 }
-
 
 export default connect(null, mapDispatchToProps)(PrimarySearchAppBar);
 
