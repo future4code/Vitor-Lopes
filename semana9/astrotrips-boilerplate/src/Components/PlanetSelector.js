e0516e01b40e8349dd42a047863b852533ace92f
vitor-slopes <vitor_slopes@hotmail.com>
2020-03-25 11:52:20 -0300

    criei estrutura para criar viagens

---
+++ semana9/astrotrips-boilerplate/src/Components/PlanetSelector.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import NativeSelect from '@material-ui/core/NativeSelect';
+
+export default function PlanetSelector() {
+  const [state, setState] = React.useState({
+    age: '',
+    name: 'hai',
+  });
+
+  const handleChange = event => {
+    const name = event.target.name;
+    setState({
+      ...state,
+      [name]: event.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <FormControl >
+        <InputLabel htmlFor="age-native-helper">Selecione o Planeta</InputLabel>
+        <NativeSelect
+          value={state.age}
+          onChange={handleChange}
+          inputProps={{
+            name: 'age',
+            id: 'age-native-helper',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option >Mercúrio</option>
+          <option>Vênus</option>
+          <option>Terra</option>
+          <option>Marte</option>
+          <option>Júpiter</option>
+          <option>Saturno</option>
+          <option>Urano</option>
+          <option>Netuno (entrou pelas cotas)</option>
+        </NativeSelect>
+      </FormControl>
+    </div>
+  );
+}
--- semana9/astrotrips-boilerplate/src/containers/CreateTripPage/index.js
+++ semana9/astrotrips-boilerplate/src/containers/CreateTripPage/index.js
@@ -3,21 +3,59 @@ import { connect } from "react-redux";
 import { push } from "connected-react-router";
 import styled from "styled-components";
 import Header from '../../Components/Header'
+import { TextField, Select } from '@material-ui/core'
+import PlanetSelector from '../../Components/PlanetSelector'
 
+const FormConteiner = styled.div`
+  width: 100%;
+  height: 80vh;
+  gap: 10px;
+  display: grid;
+  place-content: center;
+  justify-items: center;
+  grid-template-columns: 30% 30%;
+
+`
 
 class CreateTripPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+
     };
   }
+
+
 
   render() {
     return (
       <div>
         <Header />
         CreateTripPage
+        <FormConteiner>
+          <TextField
+            label="Nome da viagem"
+            id="margin-none"
+            fullWidth
+            defaultValue=""
+            helperText="Mínimo 5 caracteres"
+          />
+          <PlanetSelector />
+          <TextField
+            label="Descrição"
+            id="margin-none"
+            fullWidth
+            defaultValue=""
+            helperText="Mínimo 30 letras"
+          />
+          <TextField
+            label="Duração(em dias)"
+            id="margin-none"
+            fullWidth
+            defaultValue=""
+            helperText="Mínimo 50 dias"
+          />
+        </FormConteiner>
       </div>
     );
   }
