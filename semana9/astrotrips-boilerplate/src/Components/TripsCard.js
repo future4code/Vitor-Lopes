4b9d1ea6a3dbeec82ca91ef9d2a47e6755f97a6d
vitor-slopes <vitor_slopes@hotmail.com>
2020-03-26 12:15:00 -0300

    onectei a HomePage com a API + preparando a Store

---
+++ semana9/astrotrips-boilerplate/src/Components/TripsCard.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    maxWidth: 250,
+  },
+  media: {
+    height: 100,
+  },
+};
+
+function TripsCards(props) {
+  const { classes } = props;
+  return (
+    <Card id={props.id} onClick={() => {props.mostraPrincipal(props.id)}} className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.image}
+          title={props.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.name}
+          </Typography>
+          <Typography component="p">
+          R$ {props.price} - {props.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+      <Button size="small" color="primary">
+          Ver produto
+        </Button>
+      <Button onClick={() => {props.adicionaAoCarrinho(props.id)}} size="small" color="primary">
+          Adicionar ao carrinho
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+TripsCards.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TripsCards);
--- semana9/astrotrips-boilerplate/src/containers/HomePage/index.js
+++ semana9/astrotrips-boilerplate/src/containers/HomePage/index.js
@@ -4,16 +4,45 @@ import { push } from "connected-react-router";
 import styled from "styled-components";
 import Header from '../../Components/Header'
 import { routes } from '../../containers/Router/index'
+import axios from 'axios'
+import TripGrid from '../../Components/TripGrid'
 
-function mapDispatchToProps(dispatch) {
-  return {
-    goToTripsCreate: () => dispatch(push(routes.tripsCreate)),
-    goToTripsList: () => dispatch(push(routes.tripsList)),
-    goToTripsDetail: () => dispatch(push(routes.tripsDetails)),
-  };
-}
+const user = 'lopes'
+const baseUrl = `https://us-central1-missao-newton.cloudfunctions.net/futureX/${user}`
+
+
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  componentDidMount() {
+    this.catchTripsList()
+  }
+
+
+  catchTripsList = () => {
+    const response = axios.get(`${baseUrl}/trips`)
+    response.then((response) => {
+      const tripsList = response.data.trips
+      console.log(tripsList)
+  
+  
+      // const arraytemp = response.data.products
+      // arraytemp.forEach((item, index, array) => {
+      //   item.carrinho = 0
+      //   item.image = (`https://i.picsum.photos/id/1/${this.gna()}${this.gna()}${this.gna()}/${this.gna()}${this.gna()}${this.gna()}.jpg`)
+      // })
+      // this.setState({ products: arraytemp })
+  
+    }).catch((error) => {
+      console.log(error.message)
+    })
+  }
+  
 
   render() {
     return (
@@ -23,9 +52,18 @@ class HomePage extends Component {
         <button onClick={this.props.goToTripsCreate}>ir para TripsCreate</button>
         <button onClick={this.props.goToTripsList}>ir para goToTripsList</button>
         <button onClick={this.props.goToTripsDetail}>ir para goToTripsDetail</button>
+        <TripGrid />
       </div>
     );
   }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    goToTripsCreate: () => dispatch(push(routes.tripsCreate)),
+    goToTripsList: () => dispatch(push(routes.tripsList)),
+    goToTripsDetail: () => dispatch(push(routes.tripsDetails)),
+  };
 }
 
 export default connect(null, mapDispatchToProps)(HomePage);
