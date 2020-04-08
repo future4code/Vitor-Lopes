9f63206529197762ca47f99625ed72bd0d15ca6b
vitor-slopes <vitor_slopes@hotmail.com>
2020-04-08 12:45:33 -0300

    boilerplate instalado e configurado

---
+++ semana13/planner-sagan-vitor/src/containers/Planner/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class Planner extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default connect()(Planner);
