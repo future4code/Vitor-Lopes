710ac968565e3b5ed482e390fee2484430b09e40
vitor-slopes <vitor_slopes@hotmail.com>
2020-04-08 16:39:54 -0300

    dados da API recebidos no container, falta estruturar a página

--- semana13/planner-sagan-vitor/src/containers/Planner/index.js
+++ semana13/planner-sagan-vitor/src/containers/Planner/index.js
@@ -1,10 +1,47 @@
 import React from "react";
 import { connect } from "react-redux";
+import { createTask, getTasksList } from '../../actions'
 
 class Planner extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tasks: {}
+    }
+  }
+
+  componentDidMount() {
+    this.props.getTasks()
+  }
+
   render() {
-    return <div />;
+    console.log(this.props.taskList, 'this.props.taskList')
+
+    return (
+      <div>
+        <h1>teste</h1>
+        {this.props.taskList.map(element => {
+          return (
+            <div key={element.id}>
+              <p>{element.day}</p>
+              <p>{element.text}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
   }
 }
 
-export default connect()(Planner);
+function mapStateToProps(state) {
+  return {
+    taskList: state.tasks.tasksList
+  }
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    getTasks: () => dispatch(getTasksList())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Planner);
