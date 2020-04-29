a945b462ec679563c8a029ac1dfb54cb457d8e81
vitor-slopes <vitor_slopes@hotmail.com>
2020-04-29 15:41:19 -0300

    exercício 02 - concluído

--- semana15/dia03-Polimorfismo/src/index.ts
+++ semana15/dia03-Polimorfismo/src/index.ts
@@ -1,16 +1,26 @@
 import{Client} from './Client'
+import {Place} from './Place'
 
-const newClient: Client = {
-    name: "Vitor",
-    registrationNumber: 1,
-    consumedEnergy: 1000,
+// const newClient: Client = {
+//     name: "Vitor",
+//     registrationNumber: 1,
+//     consumedEnergy: 1000,
   
-    calculateBill: () => {
-      return 2;
-    }
-  }
-  console.log(newClient.calculateBill())
-  console.log(newClient.name)
-  console.log(newClient.registrationNumber)
-  console.log(newClient.consumedEnergy)
-//1-a) consegui imprimir todas as propriedades
+//     calculateBill: () => {
+//       return 2;
+//     }
+//   }
+//   console.log(newClient.calculateBill())
+//   console.log(newClient.name)
+//   console.log(newClient.registrationNumber)
+//   console.log(newClient.consumedEnergy)
+//1-a) consegui imprimir todas as propriedades pois não tem encapsulamento(são públicas)
+
+// const newPlace: Place = new Place()
+//2-a) Cannot create an instance of an abstract class
+
+// class newPlace extends Place {
+
+// }
+
+//2-b) Criar uma classe como filha direta da classe Place
