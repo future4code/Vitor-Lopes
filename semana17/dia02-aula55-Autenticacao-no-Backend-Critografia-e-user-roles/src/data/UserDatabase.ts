4301a1a92f70789bd30fd9c5422c41833090e3c9
vitor-slopes <vitor_slopes@hotmail.com>
2020-05-12 19:49:39 -0300

    exercícios concluídos até o 5

---
+++ semana17/dia02-aula55-Autenticacao-no-Backend-Critografia-e-user-roles/src/data/UserDatabase.ts
@@ -0,0 +1,58 @@
+import knex from "knex";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase extends BaseDatabase {
+  private connection = knex({
+    client: "mysql",
+    connection: {
+      host: process.env.DB_HOST,
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE_NAME,
+    },
+  });
+
+  private static TABLE_NAME = "User";
+
+  public async createUser(
+    id: string,
+    email: string,
+    password: string
+  ): Promise<void> {
+    await this.connection
+      .insert({
+        id,
+        email,
+        password,
+      })
+      .into(UserDatabase.TABLE_NAME);
+  }
+
+  public async getUserByEmail(email: string): Promise<any> {
+    const result = await this.getConnection()
+      .select("*")
+      .from(UserDatabase.TABLE_NAME)
+      .where({ email });
+
+    return result[0];
+  }
+
+  public async getUserById(id: string): Promise<any> {
+    const result = await this.getConnection()
+      .select("*")
+      .from(UserDatabase.TABLE_NAME)
+      .where({ id });
+
+    return result[0];
+  }
+
+  public async deleteUser(id: string): Promise<any> {
+    await this.connection
+      .delete()
+      .from(UserDatabase.TABLE_NAME)
+      .where({ id });
+  }
+}
+
+
